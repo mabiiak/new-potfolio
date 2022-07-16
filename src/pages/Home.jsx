1ebf5c354fda47cea10fb7cb416380fdf0fd693a
mabiiak <mabiane.polniak@gmail.com>
2022-07-15 23:39:38 -0300

    feat: create card with descriptive texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,18 +8,28 @@ function Home() {
     <>
       <div>
         <img alt='Foto de Mabiane' src={picture} width="100px" />
-        <h1>
-          Mabiane Polniak
-        </h1>
         <h3>
-          Desenvolvedora Front-End
+          Mabiane Polniak
         </h3>
+        <h4>
+          Desenvolvedora Front-End
+        </h4>
         <a href='https://github.com/mabiiak' target="_blank">
           <img alt='icone do gitbub' src={iconGit} width="100px" />
         </a>
         <a href='https://www.linkedin.com/in/mabianepolniak/' target="_blank">
           <img alt='icone do linkedin' src={iconLinkedin} width="100px" />
         </a>
+      </div>
+      <div>
+        <h1>Olá</h1>
+        <p>
+          Meu nome é Mabiane, moro em Curitiba. 
+          Já trabalhei com atendimento ao cliente e vendas, e com isso descobri que eu gosto de ajudar outras pessoas. <br/><br/>
+
+          Atualmente sou Desenvolvedora Fron-End e estudo Desenvolvimento Web na Trybe.
+        </p>
+        <button>Projetos</button>
       </div>
     </>
   );
